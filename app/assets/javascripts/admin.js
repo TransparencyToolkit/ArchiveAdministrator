1765af114894cd8ae41367bfc85d8a87bd1ce0ae
Brennan Novak <bnvk@transparencytoolkit.org>
2019-04-29 13:47:29 -0400

    improving Publish Archive form

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -4,15 +4,24 @@ $(document).ready(function() {
         var id = $(this).attr('id')
         if ($(this).is(':checked')) {
             $('#settings_' + id).removeClass('hidden')
+            $(this).val(1)
         } else {
             $('#settings_' + id).addClass('hidden')
+            $(this).val(0)
         }
+        console.log('set ' + id  + ' to: ' + $(this).val())
+    })
+
+    $('.choose-publication-filter').each(function() {
+        var id = $(this).attr('id').replace('filterfield_', '')
+        console.log('Show checkboxes: ' + id + '-' + $(this).val() )
+        $('#list-' + id + '-' + $(this).val()).removeClass('hidden')
     })
 
     $('.choose-publication-filter').on('change', function() {
         console.log('choose-publication-filter: list-' + $(this).val())
         $('.list-publication-filters').addClass('hidden')
-        $('#list-' + $(this).val()).removeClass('hidden')
+        $('#list-' + $(this).attr('id') + '-' + $(this).val()).removeClass('hidden')
     })
 
 })
